8ab63730e0b089de418d4897c7e61924fb732408
yoganandan <yoga@teachedison.com>
2022-08-08 18:56:44 +0530

    added fabricjs canvas

--- pages/fabric.js
+++ pages/fabric.js
@@ -1,29 +1,29 @@
 import React from "react";
 // import { fabric } from "fabric";
-// import {
-//   FabricJSCanvas,
-//   FabricJSEditorHook,
-//   useFabricJSEditor,
-// } from "fabricjs-react";
+import {
+  FabricJSCanvas,
+  FabricJSEditorHook,
+  useFabricJSEditor,
+} from "fabricjs-react";
 
 // const ComponentWithNoSSR = dynamic(() => <>No SSR</>, { ssr: false });
 
 export default function Fabric() {
   // const { editor, onReady } = useFabricJSEditor();
 
-  // return (
-  //   <>
-  //     <ComponentWithNoSSR />
-  //     {typeof window !== "undefined" && (
-  //       <FabricJSCanvas
-  //         className="fabricCanvas tw-shadow tw-border tw-border-solid tw-border-slate-300"
-  //         onReady={onReady}
-  //       />
-  //     )}
-  //   </>
-  // );
+  return (
+    <>
+      {/* <ComponentWithNoSSR /> */}
+      {typeof window !== "undefined" && (
+        <FabricJSCanvas
+          className="fabricCanvas tw-shadow tw-border tw-border-solid tw-border-slate-300"
+          onReady={onReady}
+        />
+      )}
+    </>
+  );
 
-  return <canvas>Yoga</canvas>;
+  // return <canvas>Yoga</canvas>;
 }
 
 // Fabric.getInitialProps = () => {
