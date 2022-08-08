5e22e4075e75b6ddf24be6049d1c28a8269a12b2
yoganandan <yoga@teachedison.com>
2022-08-08 19:11:03 +0530

    WIP

--- next.config.js
+++ next.config.js
@@ -1,19 +1,19 @@
 /** @type {import('next').NextConfig} */
 
-if (
-  process.env.LD_LIBRARY_PATH == null ||
-  !process.env.LD_LIBRARY_PATH.includes(
-    `${process.env.PWD}/node_modules/canvas/build/Release:`
-  )
-) {
-  process.env.LD_LIBRARY_PATH = `${
-    process.env.PWD
-  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ""}`;
-}
+// if (
+//   process.env.LD_LIBRARY_PATH == null ||
+//   !process.env.LD_LIBRARY_PATH.includes(
+//     `${process.env.PWD}/node_modules/canvas/build/Release:`
+//   )
+// ) {
+//   process.env.LD_LIBRARY_PATH = `${
+//     process.env.PWD
+//   }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ""}`;
+// }
 
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-};
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// };
 
 module.exports = nextConfig;
