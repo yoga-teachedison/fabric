bce638ee2d3babe91b6b57c291100986f8aa39e8
yoganandan <yoga@teachedison.com>
2022-08-08 19:37:28 +0530

    wip

--- pages/fabric.js
+++ pages/fabric.js
@@ -1,29 +1,22 @@
 import React from "react";
 // import { fabric } from "fabric";
-// import {
-//   FabricJSCanvas,
-//   FabricJSEditorHook,
-//   useFabricJSEditor,
-// } from "fabricjs-react";
+import {
+  FabricJSCanvas,
+  FabricJSEditorHook,
+  useFabricJSEditor,
+} from "fabricjs-react";
 
 // const ComponentWithNoSSR = dynamic(() => <>No SSR</>, { ssr: false });
 
 export default function Fabric() {
   // const { editor, onReady } = useFabricJSEditor();
 
-  // return (
-  //   <>
-  //     {/* <ComponentWithNoSSR /> */}
-  //     {typeof window !== "undefined" && (
-  //       <FabricJSCanvas
-  //         className="fabricCanvas tw-shadow tw-border tw-border-solid tw-border-slate-300"
-  //         onReady={onReady}
-  //       />
-  //     )}
-  //   </>
-  // );
-
-  return <div>fabric</div>;
+  return (
+    <FabricJSCanvas
+      className="fabricCanvas tw-shadow tw-border tw-border-solid tw-border-slate-300"
+      onReady={onReady}
+    />
+  );
 }
 
 Fabric.getInitialProps = () => {
